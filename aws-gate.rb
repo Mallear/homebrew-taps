ff14e8c003ade0c3eb15685dc2103988482f34d2
Adam Števko <adam.stevko@gmail.com>
2019-08-11 23:09:57 +0200

    Initial aws-gate homebrew package

---
+++ aws-gate.rb
@@ -0,0 +1,89 @@
+class AwsGate < Formula
+  include Language::Python::Virtualenv
+
+  desc "AWS SSM Session manager CLI client "
+  homepage "https://github.com/xen0l/aws-gate"
+  url "https://files.pythonhosted.org/packages/65/09/28c3a795af730d2dc0aee1455cca752b6c220fcd3f80605c94c98455744d/aws-gate-0.4.0.tar.gz"
+  sha256 "e9daea5df97d2d03f95d81a119965f6d98e9d259b8e95f7dd4161207189f64b1"
+
+  depends_on "python3"
+
+  resource "boto3" do
+    url "https://files.pythonhosted.org/packages/0d/cd/a192954c918fa601b85da9cc4ae88be2a26f45c883127bbc9aa2f6e44bb8/boto3-1.9.205.tar.gz"
+    sha256 "e184590781c127358c2d9ae1eab6607441d92fbddd88ba08b891b8c14d0bbfff"
+  end
+
+  resource "botocore" do
+    url "https://files.pythonhosted.org/packages/bd/1e/170b8113130beebc39d246f7125f3fd03d494b92877fe22ad6568f5b4469/botocore-1.12.205.tar.gz"
+    sha256 "0d6290f725a69a5950785fb058c2405e438674011eff7c11ce192b561b3c9aa6"
+  end
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/c5/67/5d0548226bcc34468e23a0333978f0e23d28d0b3f0c71a151aef9c3f7680/certifi-2019.6.16.tar.gz"
+    sha256 "945e3ba63a0b9f577b1395204e13c3a231f9bc0223888be653286534e5873695"
+  end
+
+  resource "chardet" do
+    url "https://files.pythonhosted.org/packages/fc/bb/a5768c230f9ddb03acc9ef3f0d4a3cf93462473795d18e9535498c8f929d/chardet-3.0.4.tar.gz"
+    sha256 "84ab92ed1c4d4f16916e05906b6b75a6c0fb5db821cc65e70cbd64a3e2a5eaae"
+  end
+
+  resource "docutils" do
+    url "https://files.pythonhosted.org/packages/84/f4/5771e41fdf52aabebbadecc9381d11dea0fa34e4759b4071244fa094804c/docutils-0.14.tar.gz"
+    sha256 "51e64ef2ebfb29cae1faa133b3710143496eca21c530f3f71424d77687764274"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/ad/13/eb56951b6f7950cadb579ca166e448ba77f9d24efc03edd7e55fa57d04b7/idna-2.8.tar.gz"
+    sha256 "c357b3f628cf53ae2c4c05627ecc484553142ca23264e593d327bcde5e9c3407"
+  end
+
+  resource "jmespath" do
+    url "https://files.pythonhosted.org/packages/2c/30/f0162d3d83e398c7a3b70c91eef61d409dea205fb4dc2b47d335f429de32/jmespath-0.9.4.tar.gz"
+    sha256 "bde2aef6f44302dfb30320115b17d030798de8c4110e28d5cf6cf91a7a31074c"
+  end
+
+  resource "marshmallow" do
+    url "https://files.pythonhosted.org/packages/16/16/d1785e853c3c9ae179c75be5cb086fb9a0d5bb11365d3a13f103795ec95f/marshmallow-2.20.0.tar.gz"
+    sha256 "6f027308cddb25b74f8245b833a355e65b21b362f38773b40325ff9b5da6c959"
+  end
+
+  resource "python-dateutil" do
+    url "https://files.pythonhosted.org/packages/ad/99/5b2e99737edeb28c71bcbec5b5dda19d0d9ef3ca3e92e3e925e7c0bb364c/python-dateutil-2.8.0.tar.gz"
+    sha256 "c89805f6f4d64db21ed966fda138f8a5ed7a4fdbc1a8ee329ce1b74e3c74da9e"
+  end
+
+  resource "PyYAML" do
+    url "https://files.pythonhosted.org/packages/e3/e8/b3212641ee2718d556df0f23f78de8303f068fe29cdaa7a91018849582fe/PyYAML-5.1.2.tar.gz"
+    sha256 "01adf0b6c6f61bd11af6e10ca52b7d4057dd0be0343eb9283c878cf3af56aee4"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/01/62/ddcf76d1d19885e8579acb1b1df26a852b03472c0e46d2b959a714c90608/requests-2.22.0.tar.gz"
+    sha256 "11e007a8a2aa0323f5a921e9e6a2d7e4e67d9877e85773fba9ba6419025cbeb4"
+  end
+
+  resource "s3transfer" do
+    url "https://files.pythonhosted.org/packages/39/12/150cd55c606ebca6725683642a8e7068cd6af10f837ce5419a9f16b7fb55/s3transfer-0.2.1.tar.gz"
+    sha256 "6efc926738a3cd576c2a79725fed9afde92378aa5c6a957e3af010cb019fac9d"
+  end
+
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/dd/bf/4138e7bfb757de47d1f4b6994648ec67a51efe58fa907c1e11e350cddfca/six-1.12.0.tar.gz"
+    sha256 "d16a0141ec1a18405cd4ce8b4613101da75da0e9a7aec5bdd4fa804d0e0eba73"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/4c/13/2386233f7ee40aa8444b47f7463338f3cbdf00c316627558784e3f542f07/urllib3-1.25.3.tar.gz"
+    sha256 "dbe59173209418ae49d485b87d1681aefa36252ee85884c31346debd19463232"
+  end
+
+  def install
+    virtualenv_create(libexec, "python3")
+    virtualenv_install_with_resources
+  end
+
+  test do
+    false
+  end
+end
